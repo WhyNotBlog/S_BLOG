bb8930e78b02a7c5a29739807c2c706e1b8b9e00
이진영 <ejnyg0613@naver.com>
2020-08-12 17:35:11 +0900

    삭제 오류 수정

--- webblogproject_frontend/src/components/PostView.vue
+++ webblogproject_frontend/src/components/PostView.vue
@@ -290,8 +290,6 @@ export default {
                 `delete/${this.user.id}/${article.articleid}`,
               {
                 data: { userid: this.user.id, articleid: article.articleid },
-              },
-              {
                 headers: {
                   "jwt-auth-token": this.jwtAuthToken,
                 },
