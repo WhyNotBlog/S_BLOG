c886b721aedcd865f10303248f80fe1d74d16179
박춘화 <winters0727@gmail.com>
2020-08-12 23:56:42 +0000

    Update webblogproject_frontend/src/components/PostView.vue

--- webblogproject_frontend/src/components/PostView.vue
+++ webblogproject_frontend/src/components/PostView.vue
@@ -171,14 +171,6 @@ export default {
     moblieWidth() {
       return window.innerWidth <= 500 ? "220px" : "280px";
     },
-    userId: {
-      get() {
-        return this.$store.getters.userId;
-      },
-      set(value) {
-        this.$store.dispatch("setUserId", value);
-      },
-    },
   },
   filters: {
     dateToString(date) {
@@ -240,9 +232,8 @@ export default {
     },
 
     checkLiked(article) {
-      if (this.loggedIn !== null) {
-        return this.userLiked.includes(article.articleid);
-      } else return false;
+      if (this.loggedIn !== null) return this.userLiked.includes(article.articleid);
+      else return false;
     },
 
     changeLiked(article) {
@@ -252,7 +243,7 @@ export default {
             .post(
               process.env.VUE_APP_LIKE + "regist",
               {
-                userid: this.userId,
+                userid: this.user.id,
                 articleid: article.articleid,
               },
               {
@@ -280,9 +271,9 @@ export default {
           axios
             .delete(
               process.env.VUE_APP_LIKE +
-                `delete/${this.userId}/${article.articleid}`,
+                `delete/${this.user.id}/${article.articleid}`,
               {
-                data: { userid: this.userId, articleid: article.articleid },
+                data: { userid: this.user.id, articleid: article.articleid },
                 headers: {
                   "jwt-auth-token": this.jwtAuthToken,
                 },
@@ -303,7 +294,6 @@ export default {
                       this.userLiked = res.data.data;
                     });
                 });
-            });
         }
       } else {
         this.text = "좋아요 기능을 사용하려면 로그인을 해야합니다.";
