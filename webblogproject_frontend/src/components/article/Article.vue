94b7a8c6c5616709d65eded2d40c0ea0ef20e7e2
이진영 <ejnyg0613@naver.com>
2020-08-13 15:39:23 +0900

    update 에러 수정

--- webblogproject_frontend/src/components/article/Article.vue
+++ webblogproject_frontend/src/components/article/Article.vue
@@ -21,7 +21,9 @@
     <div
       class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 font-weight-bold text-center"
       id="title"
-    >{{ article.title }}</div>
+    >
+      {{ article.title }}
+    </div>
     <hr class="my-5" />
     <div id="body">
       <div class="font-weight-bold text-center">
@@ -35,9 +37,14 @@
         </v-btn>
         | 작성일 :
         {{ article.editdate | dateToString }} | 조회수 : {{ article.hits }} |
-        <div class="d-inline-block" v-if="loggedIn !== null && userId === article.writerid">
+        <div
+          class="d-inline-block"
+          v-if="loggedIn !== null && userId === article.writerid"
+        >
           <v-btn color="black accent-4" icon @click="updateArticle()">
-            <v-icon middle color="black accent-4">mdi-file-document-edit</v-icon>
+            <v-icon middle color="black accent-4"
+              >mdi-file-document-edit</v-icon
+            >
           </v-btn>
           <v-btn color="black accent-4" icon @click="deleteArticle()">
             <v-icon middle color="black accent-4">mdi-delete</v-icon>
@@ -66,7 +73,8 @@
           v-for="tag in tags"
           :key="tag.tagid"
           @click="searchTag(tag.tagname)"
-        >#{{ tag.tagname }}</v-chip>
+          >#{{ tag.tagname }}</v-chip
+        >
       </div>
       <div
         class="text-xl-body-1 text-lg-body-1 text-md-body-1 text-sm-body-2 text-body-2 text-center my-5"
@@ -226,6 +234,15 @@ export default {
         this.$store.dispatch("setFollowingList", value);
       },
     },
+
+    updateArticleId: {
+      get() {
+        return this.$store.getters.updateArticleId;
+      },
+      set(value) {
+        this.$store.dispatch("setUpdateArticleId", value);
+      },
+    },
   },
   methods: {
     checkFollow() {
@@ -291,7 +308,12 @@ export default {
       this.$router.push("/search/tag/" + tag);
     },
     updateArticle() {
-      this.$router.push({ name: "Update" });
+      this.updateArticleId = this.articleId;
+
+      this.$router.push({
+        name: "Update",
+        props: { articleId: this.articleId },
+      });
     },
     deleteArticle() {
       axios
